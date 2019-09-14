e54b86638a132f5abcf866facfca73cfa4a26951
Your Name <you@example.com>
2019-09-14 18:37:23 +0600

    first commit

---
+++ src/studentdatebaseapp/StudentDataBaseApp.java
@@ -0,0 +1,10 @@
+package studentdatebaseapp;
+
+public class StudentDataBaseApp {
+    public static void main(String[] args) {
+        Student stu1 = new Student();
+        stu1.enroll();
+        stu1.payTuition();
+        System.out.println(stu1.toString());
+    }
+}
